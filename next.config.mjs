26b7c337ff05a2c9d2cf8bca5f873593241f11e3
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2024-11-18 02:07:39 -0300

    atualização de cors

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { hostname } from 'os';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
     images: {
         remotePatterns: [
             {
@@ -13,8 +14,22 @@ const nextConfig = {
                 hostname: 'images.prismic.io'
             }
         ]
+    },
+    async headers(){
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // Colocar site específico que pode fazer requisições https://matriculas.fazag.edu.br
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                    { key: "Access-Control-Max-Age", value: "86400" },
+                ]
+            }
+        ]
     }
-
 };
 
 
