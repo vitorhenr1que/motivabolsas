92e681e4629b6ab3cda100f3e368846f25e99ec6
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2024-12-03 16:33:23 -0300

    alterado o services axios para motivabolsas.com.br em produção

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = process.env.NEXT_PUBLIC_VERSION === "production" ? "https://motivabolsas-ba.vercel.app" : "http://localhost:3000"
+const url = process.env.NEXT_PUBLIC_VERSION === "production" ? "https://motivabolsas.com.br" : "http://localhost:3000"
 export const api = axios.create({
     baseURL: `${url}/api`
 })
