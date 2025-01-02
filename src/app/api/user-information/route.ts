5515a481201b707751fd52fbfdc508ff8f2ba4f1
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-01-02 16:35:03 -0300

    status do usuário user-information

---
+++ src/app/api/user-information/route.ts
@@ -0,0 +1,41 @@
+
+import { prisma } from "@/app/services/prisma";
+import { cookies } from "next/headers";
+
+export async function POST(request: Request){
+
+    const {email} = await request.json()
+    console.log('entrou no e-mail:: ', email)
+    //Compare email
+ try{
+    const userinfo = await prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+        select:{
+            id: true,
+            birthDate: true,
+            cpf: true,
+            createdAt: true,
+            email: true,
+            name: true,
+            currentPayment: true,
+            customerId: true,
+            addresses: true
+        },
+ 
+    })
+
+    return Response.json(userinfo)
+ }catch(e){
+    return Response.json({error: 'Usuário Inválido'}, {status: 400})
+ }
+    
+
+    
+
+    
+  
+
+    
+}
