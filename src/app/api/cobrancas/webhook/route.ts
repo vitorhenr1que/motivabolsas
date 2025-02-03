0da84874474422dcd8338307ecdc27f8dc7508f1
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-02-03 20:22:51 -0300

    Adicionado webhook ao site, primeira tentativa

---
+++ src/app/api/cobrancas/webhook/route.ts
@@ -0,0 +1,41 @@
+import { api } from "@/app/services/api"
+import { getInterToken } from "@/app/services/inter-token"
+import axios from "axios"
+import fs from 'fs'
+import https from 'https'
+
+const contaCorrente = process.env.INTER_ACCOUNT
+const cert = process.env.CERT_PATH
+const key = process.env.KEY_PATH
+const version = process.env.NEXT_PUBLIC_VERSION
+export async function POST(req: Request){
+ 
+    
+    
+        try{
+            
+            const agent = new https.Agent({
+                cert: fs.readFileSync(`${cert}`),
+                key: fs.readFileSync(`${key}`)
+            })
+
+            const interToken = await getInterToken()
+
+            const response = await axios.put("https://cdpj.partners.bancointer.com.br/cobranca/v3/cobrancas/webhook",{
+                webhookUrl: version === 'dev' ? 'https://localhost:3000/api/cobrancas/webhook' : 'https://motivabolsas.com.br/api/cobrancas/webhook'
+            } ,{
+                httpsAgent: agent,
+                
+                headers: {
+                    "Authorization": `Bearer ${interToken}`,
+                    "x-conta-corrente": `${contaCorrente}`,
+                    "Content-Type": "application/json"
+                }
+            })
+            
+            return Response.json(response.data, {status: 200})
+        }catch(e){
+            return Response.json({error: e}, {status: 400})
+        }
+       
+}
