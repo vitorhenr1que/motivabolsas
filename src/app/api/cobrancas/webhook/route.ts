710aae75a1c2ff6acf8097cbd384104314542856
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-02-04 02:01:07 -0300

    adicionado certificado ca base64 a variavel de ambiente e ao webhook

--- src/app/api/cobrancas/webhook/route.ts
+++ src/app/api/cobrancas/webhook/route.ts
@@ -8,37 +8,40 @@ const contaCorrente = process.env.INTER_ACCOUNT
 const version = process.env.NEXT_PUBLIC_VERSION
 const sslCert = process.env.SSL_CERT_BASE64
 const sslKey = process.env.SSL_KEY_BASE64
+const sslCa = process.env.SSL_CA_BASE64
 
 export async function POST(req: Request){
  
     
     
         try{
-            if(!sslCert || !sslKey){
+            if(!sslCert || !sslKey || !sslCa){
                 return Response.json("Certificados não encontrados.", {status: 500})
             }
             // Converter Base64 de volta para String
             const cert = Buffer.from(sslCert, "base64").toString("utf-8");
             const key = Buffer.from(sslKey, "base64").toString("utf-8");
-    
+            const ca = Buffer.from(sslCa, "base64").toString("utf-8");
             // 🔹 (Opcional) Criar arquivos temporários para APIs que exigem caminhos físicos
             const certPath = "/tmp/interCert.crt";
             const keyPath = "/tmp/privateKey.key";
-            
+            const caPath = "/tmp/ca.crt";
             // Adiciona ao arquivo temporário certPath e keyPath o conteúdo do cert e key (certPath > cert)
             fs.writeFileSync(certPath, cert);
             fs.writeFileSync(keyPath, key);
+            fs.writeFileSync(caPath, ca);
     
             const agent = new https.Agent({
                 cert: fs.readFileSync(`${certPath}`),
-                key: fs.readFileSync(`${keyPath}`)
+                key: fs.readFileSync(`${keyPath}`),
+                ca: fs.readFileSync(`${caPath}`)
           
             })
 
             const interToken = await getInterToken()
 
             const response = await axios.put("https://cdpj.partners.bancointer.com.br/cobranca/v3/cobrancas/webhook",{
-                webhookUrl: version === 'dev' ? 'https://localhost:3000/api/cobrancas/webhook' : 'https://www.motivabolsas.com.br/api/cobrancas/webhook'
+                webhookUrl: 'https://www.motivabolsas.com.br/api/cobrancas/webhook'
             } ,{
                 httpsAgent: agent,
                 
