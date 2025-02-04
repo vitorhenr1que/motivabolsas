4d37c79bb98fa95d2b10861d9194df31c7eb5716
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-02-03 21:08:00 -0300

    alterações do path de certificados das apis do inter

--- src/app/api/boletos/create/route.ts
+++ src/app/api/boletos/create/route.ts
@@ -1,10 +1,13 @@
 import axios from "axios"
 import fs from 'fs'
 import https from 'https'
+import path from "path"
 
 const contaCorrente = process.env.INTER_ACCOUNT
 const cert = process.env.CERT_PATH
 const key = process.env.KEY_PATH
+const certPath = path.join(process.cwd(), `${cert}`);
+const keyPath = path.join(process.cwd(), `${key}`);
 
 export async function POST(req: Request){
     function getNextFiveDays(){
@@ -18,8 +21,8 @@ export async function POST(req: Request){
    
         try{
             const agent = new https.Agent({
-                cert: fs.readFileSync(`${cert}`),
-                key: fs.readFileSync(`${key}`)
+                cert: fs.readFileSync(`${certPath}`),
+                key: fs.readFileSync(`${keyPath}`)
             })
             const response = await axios.post("https://cdpj.partners.bancointer.com.br/cobranca/v3/cobrancas",{
                 seuNumero: cpf, // Gerar número aleatório com 15 dígitos
--- src/app/api/cobrancas/webhook/route.ts
+++ src/app/api/cobrancas/webhook/route.ts
@@ -3,11 +3,14 @@ import { getInterToken } from "@/app/services/inter-token"
 import axios from "axios"
 import fs from 'fs'
 import https from 'https'
+import path from 'path'
 
 const contaCorrente = process.env.INTER_ACCOUNT
 const cert = process.env.CERT_PATH
 const key = process.env.KEY_PATH
 const version = process.env.NEXT_PUBLIC_VERSION
+const certPath = path.join(process.cwd(), `${cert}`);
+const keyPath = path.join(process.cwd(), `${key}`);
 export async function POST(req: Request){
  
     
@@ -15,8 +18,8 @@ export async function POST(req: Request){
         try{
             
             const agent = new https.Agent({
-                cert: fs.readFileSync(`${cert}`),
-                key: fs.readFileSync(`${key}`)
+                cert: fs.readFileSync(`${certPath}`),
+                key: fs.readFileSync(`${keyPath}`)
             })
 
             const interToken = await getInterToken()
