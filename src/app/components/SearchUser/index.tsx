ee93eff9dbf4b78ab1133e469958a17907c197d1
Vitor Henrique <vitor@MacBook-Air-de-Vitor.local>
2025-02-20 14:47:13 -0300

    adicionado campo de busca de usuários ao sistema de usuários

---
+++ src/app/components/SearchUser/index.tsx
@@ -0,0 +1,80 @@
+'use client'
+import { api } from "../../../app/services/api"
+import { useEffect, useMemo, useState } from "react"
+import styles from './style.module.scss'
+import { Loading } from "../Loading"
+import { PiMagnifyingGlassBold } from "react-icons/pi";
+import { PiTrashBold } from "react-icons/pi";
+
+interface addressProps{
+    cep: string,
+    city: string,
+    complement: string,
+    id: string,
+    neighborhood: string,
+    number: string,
+    street: string,
+    uf: string,
+    userId: string
+}
+
+interface userDataProps{
+birthDate: Date | null,
+cpf: string,
+createdAt: Date,
+email: string,
+id: string,
+name: string,
+phone: string,
+currentPayment: boolean,
+customerId: string
+addresses: addressProps[]
+}
+
+interface searchUserProps{
+    searchUsers: userDataProps[] | undefined;
+    setSearchUsers: React.Dispatch<React.SetStateAction<string | undefined>> | any;
+    onlyPaid: boolean;
+    adminKey: string;
+}
+
+export function SearchUser({searchUsers, setSearchUsers, onlyPaid, adminKey}: searchUserProps){
+
+    const [loading, setLoading] = useState(false)
+    const [searchTerm, setSearchTerm] = useState("")
+
+    async function handleClickFind(name: string){
+        try{
+            setLoading(true)
+            
+            const response = await api.post('find-users', {  
+                secret_key: adminKey,
+                onlyPaid: onlyPaid,
+                name: searchTerm.toLowerCase()
+            })
+            setSearchUsers(response.data)
+            setLoading(false)
+        } catch(e: any){
+            console.log(e)
+            setLoading(false)
+            alert(e.response.data.error)
+        }
+
+        
+    }
+    console.log(searchUsers)
+    console.log(searchTerm)
+    return(
+        <div className={styles.container}>
+         <input
+        type="text"
+        placeholder="Buscar por Nome ou CPF"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className={styles.input}
+      />
+      <button className={styles.findButton} onClick={() => handleClickFind(searchTerm)}>{loading ? <Loading/> : <PiMagnifyingGlassBold size={24}/>}</button>
+      <button className={styles.clearButton} onClick={() => setSearchUsers(undefined)}>{<PiTrashBold size={24}/>}</button>
+        </div>
+    )
+}
--- src/app/usuarios/page.tsx
+++ src/app/usuarios/page.tsx
@@ -11,6 +11,7 @@ import { Loading } from "../components/Loading";
 import { useUser } from "../components/contexts/user-provider";
 import { getInterToken } from "../services/inter-token";
 import { ModalBoleto } from "../components/ModalBoleto";
+import { SearchUser } from "../components/SearchUser";
 
 interface addressProps{
         cep: string,
@@ -41,10 +42,12 @@ export default function Usuarios(){
 const [adminKey, setAdminKey] = useState("")
 const [actualPage, setActualPage] = useState(0)
 const [users, setUsers] = useState<userDataProps[]>()
+const [searchUsers, setSearchUsers] = useState<userDataProps[]>()
 const [showInformation, setShowInformation] = useState<String>("")
 const [toggle, setToggle] = useState(false)
 const [loading, setLoading] = useState(false)
 const [onlyPaid, setOnlyPaid] = useState(false)
+const [searchTerm, setSearchTerm] = useState("")
 const response = [1,2,3,4,5,6,7,8,9,10]
 const {interToken, setInterToken} = useUser()
 
@@ -52,7 +55,9 @@ useEffect(() => {
     console.log(onlyPaid)
 },[onlyPaid])
 
-function handleClickShowInformation(id: string){
+
+
+function handleClickShowInformation(id: string){ // Toggle para o Botão de "Mostrar Informações"
     setToggle(!toggle)
     setShowInformation(toggle ? id : "")
 }
@@ -70,11 +75,12 @@ const humanizedDate = formatter.format(date);
 return humanizedDate
 }
 
-console.log(users)
+console.log(searchTerm)
     async function handleClickFind(page: number){
         try{
             setLoading(true)
-            const response = await api.post('usuarios', {
+            
+            const response = await api.post('usuarios', {  
                 secret_key: adminKey,
                 page: page,
                 onlyPaid: onlyPaid,
@@ -94,6 +100,10 @@ console.log(users)
 
 
     async function handleClickPagination( page: number){
+        if(searchUsers){
+            alert('É necessário limpar a busca de usuários para alterar entre as páginas, clique no ícone de lixeira.')
+            return
+        }
         setActualPage(page)
         handleClickFind(page)
         }
@@ -124,9 +134,61 @@ console.log(users)
                 </div>
             </div>
             <div className={styles.userList}>
-                {users !== undefined && <div className={styles.userListContainer}>{users.map((index, position) => {
+              
+                {users !== undefined && <div className={styles.userListContainer}>
+                    <SearchUser searchUsers={searchUsers} setSearchUsers={setSearchUsers} onlyPaid={onlyPaid} adminKey={adminKey}/>
+                    {!!searchUsers && searchUsers.map((index, position) => { // Quando existirem usuários buscados mostre isso:
+                        return(
+                            <div key={index.id} className={styles.userContainer}>
+                            <div className={styles.userSimpleContainer}>
+                                <div className={styles.spanContainer}>
+                                    <span className={styles.name}>{index.name.toLocaleUpperCase()}</span>
+                                    <span className={styles.cpf}>{index.cpf}</span>
+                                </div>
+                                <button className={styles.showButton} onClick={() => handleClickShowInformation(index.id)}><MdKeyboardArrowDown size={24}/><span>Mostrar Informações</span></button>
+                            </div>
+                            {showInformation === index.id && <div className={styles.showInformationContainer}>
+                            
+                            <InfoCamp campName={"Id: "} campValue={index.id} />
+                            <InfoCamp campName={"Nome: "} campValue={index.name.toLocaleUpperCase()} />
+                            <InfoCamp campName={"CPF: "} campValue={index.cpf} />
+                            <InfoCamp campName={"E-mail: "} campValue={index.email} />
+                            <InfoCamp campName={"Telefone: "} campValue={index.phone} />
+                            <InfoCamp campName={"Rua: "} campValue={index.addresses[0].street} />
+                            <InfoCamp campName={"Número: "} campValue={index.addresses[0].number} />
+                            <InfoCamp campName={"Complemento: "} campValue={index.addresses[0].complement} />
+                            <InfoCamp campName={"Bairro: "} campValue={index.addresses[0].neighborhood} />
+                            <InfoCamp campName={"CEP: "} campValue={index.addresses[0].cep} />
+                            <InfoCamp campName={"Cidade: "} campValue={index.addresses[0].city} />
+                            <InfoCamp campName={"Identificação Stipe: "} campValue={index.customerId} />
+                            <p><strong>Status da Bolsa: </strong><span className={index.currentPayment === true ? styles.statusActive : styles.statusInactive}>{index.currentPayment === true ? "Ativo" : "Inativo"}</span></p>
+                            <div className={styles.lastDiv}>
+                                <p><strong>Cadastro: </strong><span>{humanizedDate(`${index.createdAt}`)}</span></p>
+                                <ModalBoleto 
+                                    cep={index.addresses[0].cep}
+                                    city={index.addresses[0].city}
+                                    complement={index.addresses[0].complement}
+                                    cpf={index.cpf}
+                                    email={index.email}
+                                    houseNumber={index.addresses[0].number}
+                                    neighborhood={index.addresses[0].neighborhood}
+                                    name={index.name}
+                                    person="FISICA"
+                                    street={index.addresses[0].street}
+                                    uf={index.addresses[0].uf}
+                                    ddd={FormatPhone(index.phone)}
+                                    tel={FormatDdd(index.phone)}
+                                    key={index.id}
+                                />
+                            </div>
+                            
+                        </div>}
+                        </div>
+                        )
+                    })}
+                    {!searchUsers && users.map((index, position) => { // Caso não exista usuários buscado mostre isso
                     return (
-                        <div key={index.id} className={styles.userContainer}>
+                        <div key={index.id} className={styles.userContainer}> {/* Condicional para ocultar caso exista usuários buscados */}
                             <div className={styles.userSimpleContainer}>
                                 <div className={styles.spanContainer}>
                                     <span className={styles.name}>{index.name.toLocaleUpperCase()}</span>
